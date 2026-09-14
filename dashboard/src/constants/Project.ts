0e0fd607723fea763f12d4e5b51f6e6133fa9d6b
hazhanhasani <hazhanhasani4268@atomicmail.io>
2026-09-14 19:16:01 +0330

    Point BluePanel project links to fork

--- dashboard/src/constants/Project.ts
+++ dashboard/src/constants/Project.ts
@@ -1,5 +1,7 @@
-export const REPO_URL = 'https://github.com/PasarGuard/panel'
-export const ORGANIZATION_URL = 'https://github.com/PasarGuard'
-export const DONATION_URL = 'https://donate.pasarguard.org'
-export const DISCUSSION_GROUP = 'https://t.me/Pasar_Guard'
-export const DOCUMENTATION = 'https://docs.pasarguard.org'
+export const REPO_URL = 'https://github.com/hazhanhasani/panel'
+export const NODE_REPO_URL = 'https://github.com/hazhanhasani/node'
+export const ORGANIZATION_URL = 'https://github.com/hazhanhasani'
+export const DONATION_URL = REPO_URL
+export const DISCUSSION_GROUP = REPO_URL
+export const DOCUMENTATION = `${REPO_URL}#readme`
+export const INSTALL_SCRIPT_URL = 'https://raw.githubusercontent.com/hazhanhasani/panel/main/install.sh'
