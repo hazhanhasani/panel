3beb1c8431497a7442baaf3c0f8aec00783befd9
hazhanhasani <hazhanhasani4268@atomicmail.io>
2026-09-15 15:38:28 +0330

    feat(tor): register Tor models

--- app/db/__init__.py
+++ app/db/__init__.py
@@ -2,6 +2,7 @@ from sqlalchemy.ext.asyncio import AsyncSession
 
 from .base import Base, GetDB, get_db
 from .models import JWT, System, User
+from .tor_models import TorLocation, TorLocationEvent, TorSettings
 
 __all__ = [
     "JWT",
@@ -9,6 +10,9 @@ __all__ = [
     "Base",
     "GetDB",
     "System",
+    "TorLocation",
+    "TorLocationEvent",
+    "TorSettings",
     "User",
     "get_db",
 ]
